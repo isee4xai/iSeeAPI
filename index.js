1d7967424467cc72ddb4db0c2f766d238e09feb2
Chamath Palihawadana <chamathpali123@gmail.com>
2022-06-06 12:10:24 +0100

    Initiliiase iSeeAPI Project with Express and Mongoose

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
