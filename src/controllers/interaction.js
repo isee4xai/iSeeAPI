7cad67eb0fafec32f3454bc388221532716cfb0a
Chamath Palihawadana <chamathpali123@gmail.com>
2023-05-16 16:51:15 +0100

    Added Interaction Logs

--- src/controllers/interaction.js
+++ src/controllers/interaction.js
@@ -2,16 +2,22 @@ const Interaction = require("../models/interaction");
 const { v4: uuidv4 } = require("uuid");
 
 module.exports.create = async (req, res) => {
-  
+  console.log("Interaction Create --")
   let data = new Interaction({ ...req.body });
   data.company = req.companyId;
   data.user = req.userId;
   data.usecase = req.params.id;
   
+  console.log("Interaction req.params.id --", req.params.id)
+
   try {
     const result = await data.save();
-    res.status(200).json(result);
+    console.log("Interaction data.save --")
+
+    res.status(200).json({"status": true});
   } catch (error) {
+    console.log("Interaction error --", error)
+
     res
       .status(400)
       .send({
