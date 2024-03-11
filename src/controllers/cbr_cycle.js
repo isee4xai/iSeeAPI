1275bc3d528ea12e20e85fb678f33e1ad68e3477
anjanaw <shyameniw@gmail.com>
2024-03-11 10:44:23 +0000

    retain testing

--- src/controllers/cbr_cycle.js
+++ src/controllers/cbr_cycle.js
@@ -278,6 +278,7 @@ module.exports.setDefault = async (req, res) => {
 module.exports.retain = async (req, res) => {
     try {
         const usecase = await Usecase.findById(req.params.id);
+        console.log(usecase.id, usecase.version);
         const contents = await Interaction.find({ usecase: usecase.id, usecase_version: usecase.version }, ['user', 'createdAt', 'usecase_version', 'interaction']).populate('user').populate('interaction').sort({ createdAt: "desc" });
         console.log("contents.length", contents.length);
         const outcome = analyticsUtil.caseOutcome(contents);
