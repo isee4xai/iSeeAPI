ec26ed69b0e968c26e4b54a9a7122d4bb9147ee8
anjanaw <shyameniw@gmail.com>
2024-03-11 10:34:21 +0000

    retain testing

--- src/controllers/analytics.js
+++ src/controllers/analytics.js
@@ -4,7 +4,9 @@ const analyticsUtil = require("./analytics-util")
 module.exports.getAnalytics = async (req, res) => {
   try {
     const contents = await Interaction.find({ usecase: req.params.id, usecase_version: req.query.version }, ['user', 'createdAt', 'usecase_version', 'interaction']).populate('user').populate('interaction').sort({ createdAt: "desc" });
+    console.log("contents.length", contents.length);
     const analytics = analyticsUtil.analytics(contents);
+    console.log("analytics.keys", Object.keys(analytics));
     res.json(analytics);
   } catch (error) {
     res.status(500).json({ message: error.message });
